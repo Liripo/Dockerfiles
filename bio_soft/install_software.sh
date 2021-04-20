12b66a91ed0338ddf7b22a08353a99f55db1ac1b
Liripo <liripo@qq.com>
2021-04-21 00:59:34 +0800

    增加perl模块安装

--- bio_soft/install_software.sh
+++ bio_soft/install_software.sh
@@ -3,8 +3,10 @@ apt-get update && apt-get install -y --no-install-recommends \
     bedtools \
     bcftools \
     samtools \
-    seqkt \
-    vcftools
+    seqtk \
+    vcftools \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
 #fastp
 wget http://opengene.org/fastp/fastp /usr/local/bin/
 chmod a+x /usr/local/bin/fastp
@@ -14,5 +16,18 @@ tar -zxvf seqkit_linux_amd64.tar.gz /usr/local/bin
 #gatk
 unzip gatk-4.2.0.0.zip -d /opt/ && rm gatk-4.2.0.0.zip
 #sention
-wget https://s3.amazonaws.com/sentieon-release/software/sentieon-genomics-202010.tar.gz | tar -zxf - -C /opt/
-#VEP安装
+tar -zxvf sentieon-genomics-202010.tar.gz -C /opt/ && rm sentieon-genomics-202010.tar.gz
+################VEP安装,功能未安装完全
+#设置cpan镜像源
+if ! (
+    perl -MCPAN -e 'CPAN::HandleConfig->load();' \
+        -e 'CPAN::HandleConfig->prettyprint("urllist")' |
+    grep -qF 'https://mirrors.tuna.tsinghua.edu.cn/CPAN/'
+); then
+    perl -MCPAN -e 'CPAN::HandleConfig->load();' \
+        -e 'CPAN::HandleConfig->edit("urllist", "unshift", "https://mirrors.tuna.tsinghua.edu.cn/CPAN/");' \
+        -e 'CPAN::HandleConfig->commit()'
+fi
+perl -MCPAN -e "install DBI"
+cpan App::cpanminus
+cpanm Archive::Zip
